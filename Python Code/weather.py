1dc5a8172dd8697a9d2658b13fd2f009c78fb636
nathanieltan <nathaniel98.tan@gmail.com>
2017-10-25 22:13:27 -0400

    Initial weather python script

--- Python Code/weather.py
+++ Python Code/weather.py
@@ -1,14 +1,34 @@
-from weather import Weather
-weather = Weather()
+import pyowm
+import serial
 
-# Lookup WOEID via http://weather.yahoo.com.
+owm = pyowm.OWM('bcd436083a8b428965a7a71697cf6c05')  # You MUST provide a valid API key
 
-lookup = weather.lookup(560743)
-condition = lookup.condition()
-print(condition['text'])
+# # Will it be sunny tomorrow at this time in Milan (Italy) ?
+obs = owm.weather_at_place('Needham, MA')
+weatherCode = obs.get_weather().get_weather_code()
 
-# Lookup via location name.
+weatherCondition = 0
 
-location = weather.lookup_by_location('dublin')
-condition = location.condition()
-print(condition['text'])
+if 200 <= weatherCode < 300:
+    weatherCondition = 4
+elif 300 <= weatherCode < 400:
+    weatherCondition = 4
+elif 400 <= weatherCode < 500:
+    weatherCondition = 4
+elif 500 <= weatherCode < 600:
+    weatherCondition = 4
+elif 600 <= weatherCode < 700:
+    weatherCondition = 4
+elif 700 <= weatherCode < 800:
+    weatherCondition = 1
+elif weatherCode == 800:
+    weatherCondition = 1
+elif 801 <= weatherCode < 803:
+    weatherCondition = 2
+elif 803 <= weatherCode < 804:
+    weatherCondition = 3
+
+print(weatherCondition)
+ser = serial.Serial('/dev/ttyACM0', 9600)
+while True:
+    ser.write(weatherCondition)
