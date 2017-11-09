ef3a033dd03ea88b0063c091dc2f8e16aa0da0d6
camillexue <xue.camille@gmail.com>
2017-11-09 12:11:14 -0500

    added LED and Servo setup, first draft of sending state info

--- masterArduino/masterArduino.ino
+++ masterArduino/masterArduino.ino
@@ -2,17 +2,25 @@
 
 int address1 = 9; // address for 
 int x = 5;
+int state;
 
 void setup() {
   // put your setup code here, to run once:
   Wire.begin();
+
+  Serial.begin(9600);
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  while (Serial.available() > 0) {
+     state = (int)Serial.read(); //value 1-4
+     Serial.println(state);
+  }
+  
   Wire.beginTransmission(address1);
-  Wire.send(x);
+  Wire.write(state);
   Wire.endTransmission();
   delay(500);
 }
--- slaveArduino/slaveArduino.ino
+++ slaveArduino/slaveArduino.ino
@@ -1,23 +1,59 @@
 #include <Wire.h>
+#include <Servo.h>
 
 int I2C_address = 9;
+int pin1 = A0;
+
+int pos;
+int state = 1;
+
+Servo ball[10];
 
 void setup() {
   // put your setup code here, to run once:
   Wire.begin(I2C_address);
   Wire.onReceive(receiveEvent);
-  Serial.begin(9600);
+
+  for (int i = A0; i <= A4; i++) {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW);
+  }
+
+  for (int i = 0; i < 10; i++) {
+    ball[i].attach(i+2);
+    ball[i].write(0);
+  }
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  switch(state) {
+    case 1: //sunny
+      pos = 175;
+      break;
+    case 2: //partly cloudy
+      pos = 145;
+      break;
+    case 3: //cloudy
+      pos = 110;
+      break;
+    case 4: //rainy
+      pos = 80;
+      break;
+     default: //nothing
+      pos = 185;
+      break;
+  }
+
+  for (int i = 0; i < 10; i++) {
+    ball[i].write(pos);
+  }
 }
 
 void receiveEvent() {
   while(Wire.available()) {
-    int x = Wire.read();
-    Serial.print(x);
+    state = Wire.read();
   }
 }
 
