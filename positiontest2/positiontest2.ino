73210a947c2cd7339f55f30496d84ffd1184bd43
camillexue <xue.camille@gmail.com>
2017-12-08 10:14:18 -0500

    Updated position test

--- positiontest2/positiontest2.ino
+++ positiontest2/positiontest2.ino
@@ -34,6 +34,10 @@ void setup() {
   Wire.begin();
   Serial.begin(9600);
   moveToFive();
+  //cloudy();
+  //dynamicCloudy();
+  delay(10000);
+  reset();
 }
 
 void loop() {
@@ -74,9 +78,10 @@ void reset() {
 void moveToFive() {
   for (int j = 0; j < 6; j++) {
     for (int k = 0; k < 4; k++) {
-      allPositions[j][k] = 5;
+      allPositions[j][k] = 10;
     }
   }
+  delay(200);
   updateAll();
 }
 
@@ -87,17 +92,24 @@ void updateAll() {
         address = motorAddresses[j][k];
         motorNumber = address % 10;
         arduinoNumber = (address - motorNumber) / 10;
-        positions[arduinoNumber-1][motorNumber] = allPositions[j][k];
+        positions[arduinoNumber-1][motorNumber-1] = allPositions[j][k];
       }
     }
     byte specificPositions[4];
-    for(int x = 0; x < 1; x++){
+    for(int x = 0; x < 6; x++){
       for (int y = 0; y < 4; y++){
         specificPositions[y]=positions[x][y];
       }
       Wire.beginTransmission(slaves[x]);
       Wire.write(specificPositions, 4);
       Wire.endTransmission();     
+      Serial.print(specificPositions[0]);
+      Serial.print(" ");
+      Serial.print(specificPositions[1]);
+      Serial.print(" ");
+      Serial.print(specificPositions[2]);
+      Serial.print(" ");
+      Serial.println(specificPositions[3]);
     }
 
 }
@@ -148,3 +160,98 @@ int getValue() {
   }
   return value;
 }
+
+void sunny() {
+  byte sunnyPositions[6][4] = {{0, 0, 0, 0}, 
+                               {0, 2, 2, 0}, 
+                               {0, 3, 3, 0}, 
+                               {0, 3, 3, 0},
+                               {0, 2, 2, 0},
+                               {0, 0, 0, 0}};
+  for (int j = 0; j < 6; j++) {
+    for (int k = 0; k < 4; k++) {
+      allPositions[j][k] = sunnyPositions[j][k];
+    }
+  }
+  updateAll();
+}
+
+void rainy() {
+  byte rainyPositions[6][4] = {{7, 8, 8, 7}, 
+                               {5, 14, 6, 7}, 
+                               {6, 12, 7, 8}, 
+                               {6, 7, 13, 8},
+                               {7, 8, 7, 7},
+                               {7, 11, 7, 7}};
+  for (int j = 0; j < 6; j++) {
+    for (int k = 0; k < 4; k++) {
+      allPositions[j][k] = rainyPositions[j][k];
+    }
+  }
+  updateAll();
+}
+
+void cloudy() {
+  byte cloudyPositions[6][4] = {{7, 8, 8, 7}, 
+                               {5, 7, 6, 7}, 
+                               {6, 5, 7, 8}, 
+                               {6, 7, 6, 8},
+                               {7, 8, 7, 7},
+                               {7, 7, 7, 7}};
+  for (int j = 0; j < 6; j++) {
+    for (int k = 0; k < 4; k++) {
+      allPositions[j][k] = cloudyPositions[j][k];
+    }
+  }
+
+  
+  updateAll();
+}
+
+void partcloudy() {
+  byte cloudyPositions[6][4] = {{0, 0, 0, 0}, 
+                               {0, 1, 1, 0}, 
+                               {0, 0, 7, 0}, 
+                               {7, 7, 8, 9},
+                               {8, 5, 6, 7},
+                               {7, 6, 7, 7}};
+  for (int j = 0; j < 6; j++) {
+    for (int k = 0; k < 4; k++) {
+      allPositions[j][k] = cloudyPositions[j][k];
+    }
+  }
+  updateAll();
+}
+
+void dynamicCloudy() {
+  float pi = 3.14;
+  
+  for(float t = 0; t < (3*pi); t = t+ (pi/9)) {
+    int m = 0;
+    byte height[6];
+    for(float a = 0; a < (2*pi); a = a+ (pi/3)){
+      y = ((sin(t+a) + 1) / 2) * 14;
+      height[m] = y;
+      m++;
+    }
+    
+    for(int l = 0; l < 6; l++) {
+      allPositions[l][0] = byte(height[l]);
+    }
+//    Serial.print(height[0]);
+//    Serial.print(" ");
+//    Serial.print(height[1]);
+//    Serial.print(" ");
+//    Serial.print(height[2]);
+//    Serial.print(" ");
+//    Serial.print(height[3]);
+//    Serial.print(" ");
+//    Serial.print(height[4]);
+//    Serial.print(" ");
+//    Serial.println(height[5]);
+//    Serial.println();
+    updateAll();
+  }
+  
+}
+
